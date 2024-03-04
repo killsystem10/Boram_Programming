390de4927f4ba1621ac8703cc7cceb15e63cfdf7
killsystem10 <killsystem10@outlook.com>
2024-03-04 12:20:35 +0900

    3월 4일 정보과학 첫수업

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,11 +1,25 @@
 #include <stdio.h>
 int main() {
 	int a, b;
-	scanf_s("%d", &a);
-reget:
-	scanf_s("%d", &b);
-	if (a-- != 0) {
-		printf("%d\n", b);
-		goto reget;
+	scanf("%d %d", &a, &b);
+	if (a % 2 == 0) {
+		if (b % 2 == 0) {
+			printf("Â¦¼ö+Â¦¼ö=Â¦¼ö");
+		}
+	}
+	if (a % 2 == 0) {
+		if (b % 2 != 0) {
+			printf("Â¦¼ö+È¦¼ö=È¦¼ö");
+		}
+	}
+	if (a % 2 != 0) {
+		if (b % 2 == 0) {
+			printf("È¦¼ö+Â¦¼ö=È¦¼ö");
+		}
+	}
+	if (a % 2 != 0) {
+		if (b % 2 != 0) {
+			printf("È¦¼ö+È¦¼ö=Â¦¼ö");
+		}
 	}
 }
