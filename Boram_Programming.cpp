5bb585be03950a626b9781c1747c8a2d850ce29e
killsystem10 <killsystem10@outlook.com>
2024-03-11 11:15:34 +0900

    정보과학 수업

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,25 +1,6 @@
 #include <stdio.h>
 int main() {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	if (a % 2 == 0) {
-		if (b % 2 == 0) {
-			printf("Â¦¼ö+Â¦¼ö=Â¦¼ö");
-		}
-	}
-	if (a % 2 == 0) {
-		if (b % 2 != 0) {
-			printf("Â¦¼ö+È¦¼ö=È¦¼ö");
-		}
-	}
-	if (a % 2 != 0) {
-		if (b % 2 == 0) {
-			printf("È¦¼ö+Â¦¼ö=È¦¼ö");
-		}
-	}
-	if (a % 2 != 0) {
-		if (b % 2 != 0) {
-			printf("È¦¼ö+È¦¼ö=Â¦¼ö");
-		}
-	}
+	int temp;
+	scanf("%d", &temp);
+	printf("%d %d %d", temp,temp,temp);
 }
