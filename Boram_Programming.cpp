cae4bbc0068aa5fd54921be24a54c436dcae9d2f
killsystem10 <killsystem10@outlook.com>
2024-03-18 11:59:46 +0900

    문제 AV 해결안됨

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,25 +1,8 @@
 #include <stdio.h>
 int main() {
-	int a;
-	scanf("%d", &a);
-	switch (a) {
-	case 3:
-	case 4:
-	case 5:
-		printf("spring");
-		break;
-	case 6:
-	case 7:
-	case 8:
-		printf("summer");
-		break;
-	case 9:
-	case 10:
-	case 11:
-		printf("fall");
-		break;
-	}
-	if (a == 12 || a == 1 || a == 2) {
-		printf("winter");
-	}
+	long long int a, b, c, temp;
+	scanf("%lld %lld %lld", &a, &b, &c);
+	printf("%lld\n", a + b + c);
+	temp = a + b + c;
+	printf("0.1f", temp / 3);
 }
