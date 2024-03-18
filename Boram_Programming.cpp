a0cb8e9a7ec96558af6efd3993d36eb0739163ca
killsystem10 <killsystem10@outlook.com>
2024-03-18 11:24:39 +0900

    정보과학 선택구조 수업

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
 int main() {
-	long long int a, b;
-	scanf("%lld %lld", &a, &b);
-	printf("%lld", a + b);
+	int a;
+	scanf("%d", &a);
+	switch (a) {
+	case 3:
+	case 4:
+	case 5:
+		printf("spring");
+		break;
+	case 6:
+	case 7:
+	case 8:
+		printf("summer");
+		break;
+	case 9:
+	case 10:
+	case 11:
+		printf("fall");
+		break;
+	}
+	if (a == 12 || a == 1 || a == 2) {
+		printf("winter");
+	}
 }
