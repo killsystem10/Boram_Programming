915d4310115fed37da0ddd987561b69ee7fa3d93
killsystem10 <killsystem10@outlook.com>
2024-03-25 12:20:44 +0900

    3월 25일 정보과제수업

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-	long long int a, b, c, temp;
-	scanf("%lld %lld %lld", &a, &b, &c);
-	printf("%lld\n", a + b + c);
-	temp = a + b + c;
-	printf("0.1f", temp / 3);
+	int a;
+	scanf("%d", &a);
+	for (int i = 0; i <= a; i++) {
+		printf("%d\n", i);
+	}
 }
