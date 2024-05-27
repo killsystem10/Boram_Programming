c4984426eb1de5c8453c63bac4856e5b7d385298
killsystem10 <killsystem10@outlook.com>
2024-05-27 11:22:53 +0900

    정보과학 알고리즘 수업

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#define MAXN 9
-int main()
-{
-	int i, ans, index, A[MAXN + 1];
-	ans = 0;
-	index = 0;
-	for (i = 1; i < MAXN + 1; i++)
-		scanf("%d", &A[i]);
-	for (i = 1; i < MAXN + 1; i++)
-		if (ans < A[i])
-		{
-			ans = A[i];
-			index = i;
-		}
-	printf("%d\n%d\n", ans, index);
-	return 0;
+
+int main() {
+	int i, a, sum = 0;
+	scanf("%d", &a);
+	for (i = 1; i <= a; i++) {
+		sum = sum + i;
+	}
+	printf("%d", sum);
 }
