24f8274e5add4687e7bceb3a3b228424dd5d55c0
killsystem10 <killsystem10@outlook.com>
2024-06-20 11:48:40 +0900

    재귀함수

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int a,cnt;
-double xt, yt;
-int main() {
-	srand(time(NULL));
-	for (int i = 0; i <= 32767; i++) {
-		xt = (double)(rand()) / 32767 * 2;
-		yt = (double)(rand()) / 32767 * 2;
-		a++;
-		if (xt * xt + yt + yt <= 4) {
-			cnt++;
-		}
-	}
-	printf("%lf\n", 4.0 * cnt / a);
+int print_number(int n) {
+	if (n > 3) return n;
+	printf("%d\n", n);
+	print_number(n + 1);
+}
+
+int main(void) {
+	int count = 1;
+	print_number(count);
+	return 0;
 }
