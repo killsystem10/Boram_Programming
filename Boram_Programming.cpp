95203155b961be9a61a385c3b3ca9aa9a741aca8
killsystem10 <killsystem10@outlook.com>
2024-08-28 16:06:27 +0900

    Visuao Studio 추가

--- Boram_Programming.cpp
+++ Boram_Programming.cpp
@@ -1,12 +1,4 @@
 #include <stdio.h>
-int print_number(int n) {
-	if (n > 3) return n;
-	printf("%d\n", n);
-	print_number(n + 1);
-}
-
-int main(void) {
-	int count = 1;
-	print_number(count);
-	return 0;
+int main() {
+	printf("test");
 }
